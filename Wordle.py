66ba2418bc93ed9d43275dc65740e6d808311790
mayokunade <maya.adedeji@duke.edu>
2026-04-14 18:07:33 -0400

    new pipeline

--- Wordle.py
+++ Wordle.py
@@ -1,118 +1,99 @@
-# Gets the 'random' and 'time' libraries
-import random 
+import random
 import time
-
 
 # Greets user, explains the rules
 print("Welcome to Wordle for Discrete Math!")
 time.sleep(2)
+
 username = input("What is your name? ")
 print("Hello, " + username.capitalize() + "! The rules are simple: ")
 time.sleep(2.5)
-print("Choose your language.")
-time.sleep(2)
-print("Then, enter a FIVE-LETTER word of that language.")
+
+print("Enter a FIVE-LETTER word.")
 time.sleep(2.5)
-print("The code will let you know if letters are correct") 
+
+print("The code will let you know if letters are correct")
 print("and/or correctly placed.")
 time.sleep(2.5)
+
 print("No letters are allowed to be reused in a word.")
 time.sleep(2)
 
-# Asks user if they are ready to play
+# Ask if ready
 ready = input("Are you ready, " + username.capitalize() + "? Y/N (cap-sensitive): ")
+
 while True:
-    # What to do if user inputs "Y"
+
     if ready == "Y":
-        # Asks for language
-        language = input("English or Spanish? Click anything to exit. (case-sensitive) ")
-    
-        # What to do if user inputs "English"
-        if language == "English":
-            # Opens the file in read mode ('r' signifies 'read')
-            with open("poss_words_english.txt", "r") as file:
-                allText = file.read()
-                words = list(map(str, allText.split()))
-              
-                # Chooses a random word as the Wordle
-                wordle = random.choice(words)
-        # What to do if user inputs "Spanish"
-        elif language == "Spanish":
-            # Opens the file in read mode ('r' signifies 'read')
-            with open("poss_words_spanish.txt", "r") as file:
-                allText = file.read()
-                words = list(map(str, allText.split()))
-                
-                # Chooses a random word as the Wordle
-                wordle = random.choice(words)
-        # Neither of the above scenario
-        else:
-            print("Bye!")
-            break
-        
-        # Defines the 'check for correct place' function
+
+        # Load word list from file
+        with open("wordle_dictionary_balanced.txt", "r") as file:
+            allText = file.read()
+            words = list(map(str, allText.split()))
+
+        # Pick random word
+        wordle = random.choice(words)
+
+        # Function to check correct placement
         def check_place(char_g, char_w, place):
             if char_g == char_w:
                 print(place + " letter: right letter, right place!")
-        
-        # User's first guess
+
+        # First guess
         guess = input("Enter a word: ")
-        
-        # Forces the user to enter ONLY a five-letter word
-        while (len(guess) != 5):
-            print("That was not a five letter word!")
+
+        # Ensure 5-letter word
+        while len(guess) != 5:
+            print("That was not a five-letter word!")
             guess = input("Enter a word: ")
-            
-        # For loop repeats the guesses five more times for a total of six times
+
+        # 6 total attempts (Wordle-style)
         for i in range(5):
-            
-            # Checks if the Wordle has been guessed
+
             if guess == wordle:
                 print("You guessed it!")
                 break
-              
-            # Checks correct letter guess and letter placement for place 1
+
+            # Check each position
+
             check_place(guess[0], wordle[0], "First")
-            if guess[0] == wordle[1] or guess[0] == wordle [2] or guess[0] == wordle [3] or guess[0] == wordle[4]:
+            if guess[0] in wordle[1:]:
                 print("First letter: right letter, wrong place.")
-            
-            # Checks correct letter guess and letter placement for place 2
+
             check_place(guess[1], wordle[1], "Second")
-            if guess[1] == wordle[0] or guess[1] == wordle [2] or guess[1] == wordle [3] or guess[1] == wordle[4]:
+            if guess[1] in wordle[0] + wordle[2:]:
                 print("Second letter: right letter, wrong place.")
-            
-            # Checks correct letter guess and letter placement for place 3    
+
             check_place(guess[2], wordle[2], "Third")
-            if guess[2] == wordle[0] or guess[2] == wordle [1] or guess[2] == wordle [3] or guess[2] == wordle[4]:
+            if guess[2] in wordle[:2] + wordle[3:]:
                 print("Third letter: right letter, wrong place.")
-            
-            # Checks correct letter guess and letter placement for place 4
+
             check_place(guess[3], wordle[3], "Fourth")
-            if guess[3] == wordle[0] or guess[3] == wordle [1] or guess[3] == wordle [2] or guess[3] == wordle[4]:
+            if guess[3] in wordle[:3] + wordle[4]:
                 print("Fourth letter: right letter, wrong place.")
-            
-            # Checks correct letter guess and letter placement for place 5
+
             check_place(guess[4], wordle[4], "Fifth")
-            if guess[4] == wordle[0] or guess[4] == wordle [1] or guess[4] == wordle [2] or guess[4] == wordle[3]:
+            if guess[4] in wordle[:4]:
                 print("Fifth letter: right letter, wrong place.")
-            
-            # Prompts user to keep guessing while the for loop runs
+
+            # Next guess
             guess = input("Enter a word: ")
-            while (len(guess) != 5):
-                print("That was not a five letter word!")
+
+            while len(guess) != 5:
+                print("That was not a five-letter word!")
                 guess = input("Enter a word: ")
-            
-        # Prints a "sorry" message if the user is unsuccessful
+
+        # Loss message
         if guess != wordle:
             print("You have used up your guesses. The Wordle was " + wordle + ".")
             print("Try again next time!")
-            break
-    # What to do if user inputs "N"
+
+        break
+
     elif ready == "N":
         print("Not ready yet? Press the Run button when you are!")
         break
-    # Invalid input scenario
+
     else:
         print("Invalid input.")
         break
-
